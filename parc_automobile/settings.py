63f4f701fec9a824be73e3f2b4fe870077ab61b5
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-04-09 11:46:27 +0000

    deplois

--- parc_automobile/settings.py
+++ parc_automobile/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5dplaozet82k%woepm1wcx5pb(6qj52l0eg@^$4!b@+!_)(0)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
 # ALLOWED_HOSTS = ['192.168.11.135', '127.0.0.1', '192.168.20.155']
 
@@ -60,7 +60,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ALLOWED_HOSTS = ['parc-deploi.onrender.com', 'localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 ROOT_URLCONF = 'parc_automobile.urls'
